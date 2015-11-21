0878522f4c278daf1b7d36f4c981ccc973932a11
Fyerfox <fyerfox12@gmail.com>
2015-11-21 00:03:36 -0600

    index.php
    PHP that generates HTML comments recursively.

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+	<style>
+		.comment{
+			border:black solid 1px;
+			margin:5px;
+			padding:5px;
+		}
+		.comment > div{
+			margin-left:20px;
+		}
+	</style>
+	
+<?php
+	require("/php/config.php");
+	require("/php/functions.php");
+	
+	f_sqlConnect(DB_USER, DB_PASSWORD, DB_NAME);
+	
+	echo readcomments("0");
+	
+	
+	function readcomments($parentid){
+		$returnstring = "";
+		$sql = "SELECT * FROM testcomments WHERE parentid = '" . $parentid ."'";
+		$queryresult = mysql_query($sql);
+		
+		if(!$queryresult){
+			die("Error: " . mysql_error());
+		}
+		
+		while($row = mysql_fetch_array($queryresult)){
+			$id = $row["id"];
+			$parentid = $row["parentid"];
+			$upvotes = $row["upvotes"];
+			$content = $row["content"];
+			
+			$returnedstring .= "<div class='comment'>ID: " . $id . " Parent ID: " . $parentid . " Upvotes " . $upvotes . "<p>" . $content . "</p>";
+			$returnedstring .= readcomments($id);
+		}
+		
+		return $returnedstring . "</div>";
+	}
+?>
+
+</html>
